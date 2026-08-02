20eab9d7be9ae23898646c1bfa073a997e4e9e8a
abrorhamraqulov5-png <abrorhamraqulov5@gmail.com>
2026-08-03 02:33:37 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,26 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
 
+from config import BOT_TOKEN
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def start_command(message: Message):
+    await message.answer(
+        "👋 Assalomu alaykum!\n\n"
+        "Olma ID Bot'ga xush kelibsiz.\n\n"
+        "🪪 Beydjik yaratish funksiyasi tez orada tayyor bo'ladi."
+    )
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
